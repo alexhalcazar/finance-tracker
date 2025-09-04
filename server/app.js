8f8d797bfb9673952f3e2d2d2f9dfed97b9591f4
jay.develops <swejesusperez@gmail.com>
2025-09-04 12:37:36 -0700

    Removed old routes, left barebones in app.js

--- server/app.js
+++ server/app.js
@@ -1,27 +1,9 @@
 import express from "express";
-import mongodb from "./api/db/connection.js";
-
-import users from "./api/routes/user.js";
-import mealplan from "./api/routes/mealplan.js";
-import meals from "./api/routes/meals.js";
-
 const app = express();
 const PORT = 8080;
-dotenv.config();
-
-//  enable cors on the server
-const options = { exposedHeader: ["Authorization"] };
-app.use(cors(options));
 
 app.use(express.json());
-
-app.use("/users", users);
-app.use("/mealplans", mealplan);
-app.use("/meals", meals);
-
 app.listen(PORT, async () => {
-  //  connect to mongodb before starting server
-  await mongodb.connect();
-
+  //  can use any database later on to initiate the connection here
   console.log(`Server is running on port: ${PORT}`);
 });

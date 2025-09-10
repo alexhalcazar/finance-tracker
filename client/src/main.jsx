955193a88ad64b2220592d9b3c19937250522bba
jay.develops <swejesusperez@gmail.com>
2025-09-09 20:59:41 -0700

    description: initialized routing for project in data mode.

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,10 @@
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import App from "./App.jsx";
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { RouterProvider } from "react-router/dom";
+import router from "@/router/routes"
 
-createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <App />
-  </StrictMode>
+const root = document.getElementById("root");
+
+ReactDOM.createRoot(root).render(
+  <RouterProvider router={router} />,
 );

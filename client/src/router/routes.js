045a3f2e503c02091af8c5f2910e35cbfd61a87a
jay.develops <swejesusperez@gmail.com>
2025-09-10 22:36:51 -0700

    feat: added routing to application.

--- client/src/router/routes.js
+++ client/src/router/routes.js
@@ -1,12 +1,17 @@
 import { createBrowserRouter } from "react-router";
 import App from "@/App";
+import { Login } from "@/pages/Login";
 
 const router = createBrowserRouter([
   {
-    path: "/",
-    Component: App,
     children: [
       { index: true, Component: App },
+      {
+        path: "auth",
+        children: [
+          { path: "login", Component: Login },
+        ],
+      },
     ],
   },
 ]);

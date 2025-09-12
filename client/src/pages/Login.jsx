e154be4ba6239126076bd8eec0a7ceda235d0111
jay.develops <swejesusperez@gmail.com>
2025-09-11 21:53:15 -0700

    refactor: login and register pages using tailwindcss styling convention.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,23 +1,34 @@
 import { AuthForm } from "@/components/AuthForm";
 import "@/styles/Auth.css";
 import { Link } from "react-router";
+import { Card } from "../components/ui/Card";
 
 export function Login() {
   return (
-    <main className="auth-container">
-      <div className="auth-header">
-        <h2>Login</h2>
-        <p>Welcome back to pocket budget, jump back below.</p>
-      </div>
+    <main className="min-h-screen flex items-center justify-center ">
+      <div className="max-w-md w-full space-y-8">
+        <div className="text-center space-y-2">
+          <h2 className="text-3xl text-primary font-bold">Login</h2>
+          <p className="text-base text-muted">
+            Welcome back to pocket budget, jump back below.
+          </p>
+        </div>
 
-      <div className="auth-card">
-        <AuthForm className="auth-form" formType="login" />
-        <p>
-          Don&apos;t have an account?
-          <span>
-            <Link to="/auth/register">Register</Link>
-          </span>
-        </p>
+        <Card className="p-8 space-y-6">
+          <AuthForm className="auth-form" formType="login" />
+
+          <div className="text-center">
+            <p className="text-sm text-muted">
+              Don&apos;t have an account?{" "}
+              <Link
+                to="/auth/register"
+                className="font-medium text-primary hover:text-primary/80 transition-colors"
+              >
+                Register
+              </Link>
+            </p>
+          </div>
+        </Card>
       </div>
     </main>
   );

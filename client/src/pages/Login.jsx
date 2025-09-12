5653eec61d3bf1a6404ae6a3c89d13c8bab53e91
jay.develops <swejesusperez@gmail.com>
2025-09-11 22:06:10 -0700

    chore: updated non relative imports with aliases.

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,7 @@
-import { AuthForm } from "@/components/AuthForm";
+import { AuthForm } from "@/features/auth/AuthForm";
 import "@/styles/Auth.css";
 import { Link } from "react-router";
-import { Card } from "../components/ui/Card";
+import { Card } from "@/components/ui/Card";
 
 export function Login() {
   return (

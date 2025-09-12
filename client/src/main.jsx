a6d6daa05fd3c383b7c8ac30c855f31eeb4798dc
jay.develops <swejesusperez@gmail.com>
2025-09-11 22:50:49 -0700

    chore: removed components.css import in main.jsx

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,14 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router/dom";
-import router from "@/router/routes"
+import router from "@/router/routes";
 
 // importing globals first will allow access to other descending style sheets
-import "@/styles/globals.css"
-import "@/styles/components.css"
+import "@/styles/globals.css";
 
 const root = document.getElementById("root");
 
-ReactDOM.createRoot(root).render(
-  <RouterProvider router={router} />,
-);
+ReactDOM.createRoot(root).render(<RouterProvider router={router} />);

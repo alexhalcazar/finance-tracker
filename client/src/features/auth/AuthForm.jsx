65ebc3979b5fc2242d63b3a6ef2a557fdef1ae3b
jay.develops <swejesusperez@gmail.com>
2025-09-12 21:16:29 -0700

    feat: added form validation and submission to login screen.

--- client/src/features/auth/AuthForm.jsx
+++ client/src/features/auth/AuthForm.jsx
@@ -1,38 +1,78 @@
 import { Input } from "@/components/ui/input";
 import { Button } from "@/components/ui/button";
 import { Label } from "@/components/ui/label";
+import { cn } from "@/utils/cn";
 
-export function AuthForm({ formType, className }) {
+/**
+ *
+ * @prop {formType} uses if the parent form needs login or register form
+ * @prop {form} passes the react hook form object to handle submission and validation
+ * @prop {onSubmit} passes the function on what the form will submit to the submit button
+ * @prop {isLoading} checks if the form is currently in a loading state
+ * @prop {className} optional className to style parent auth form component
+ */
+export function AuthForm({
+  formType,
+  form,
+  onSubmit,
+  isLoading,
+  className = "",
+}) {
   //  Make text be register when passed in register, otherwise it is sign in
   const buttonCta = formType === "register" ? "Register" : "Sign In";
 
-  return (
-    <form className={className}>
-      <div className="space-y-8">
-        <Label htmlFor="email">Email</Label>
-        <Input type="email" placeholder="Enter your email" id="email" />
+  //  Grab the parent form object passed from parent component, destructure their properties
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = form;
 
+  return (
+    <form className={className} onSubmit={handleSubmit(onSubmit)}>
+      <div className="space-y-2">
+        <Label htmlFor="email">Email</Label>
+        <Input
+          type="email"
+          placeholder="Enter your email"
+          id="email"
+          {...register("email")}
+        />
+        <p className={cn([errors.email?.message && "text-error"])}>
+          {errors.email?.message}
+        </p>
+      </div>
+
+      <div className="pt-6 space-y-2">
         <Label htmlFor="password">Password</Label>
         <Input
           type="password"
           placeholder="Enter your password"
           id="password"
+          {...register("password")}
         />
-
-        {formType === "register" && (
-          <>
-            <Label htmlFor="confirmPassword">Confirm Password</Label>
-            <Input
-              type="password"
-              placeholder="Confirm your password"
-              id="confirmPassword"
-            />
-          </>
-        )}
+        <p className={cn([errors.password?.message && "text-error"])}>
+          {errors.password?.message}
+        </p>
       </div>
-      <div className="mt-4">
-        <Button variant="primary" type="submit">
-          {buttonCta}
+
+      {formType === "register" && (
+        <>
+          <Label htmlFor="confirmPassword">Confirm Password</Label>
+          <Input
+            type="password"
+            placeholder="Confirm your password"
+            id="confirmPassword"
+            {...register("confirmPassword")}
+          />
+          <p className={cn([errors.password?.message && "text-error"])}>
+            {errors.confirmPassword?.message}
+          </p>
+        </>
+      )}
+      <div className="mt-6">
+        <Button variant="primary" type="submit" disabled={isLoading}>
+          {isLoading ? "Loading..." : buttonCta}
         </Button>
       </div>
     </form>

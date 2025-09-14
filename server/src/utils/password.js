ed73b0be39f5987e903dd6cb6f2689091878e5e9
KayTee <80430997+KMTrochez@users.noreply.github.com>
2025-09-13 22:04:19 -0700

    Update password.js
    minor update

--- server/src/utils/password.js
+++ server/src/utils/password.js
@@ -1,6 +1,6 @@
 import bcrypt from 'bcryptjs';
 
-const SALT_ROUNDS = 12; // good default for servers
+const SALT_ROUNDS = 12; 
 
 export async function hashPassword(plain) {
   return await bcrypt.hash(plain, SALT_ROUNDS);

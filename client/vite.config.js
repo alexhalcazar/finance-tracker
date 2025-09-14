c83d731b4c30b2e3731da71d048476125be5483e
jay.develops <swejesusperez@gmail.com>
2025-09-13 18:10:28 -0700

    chore: vitest setup for front end unit testing.

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,21 +3,32 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 import tailwindcss from "@tailwindcss/vite";
 
+const aliases = {
+  "@": path.resolve(__dirname, "./src"),
+  "@components": path.resolve(__dirname, "./src/components"),
+  "@features": path.resolve(__dirname, "./src/features"),
+  "@pages": path.resolve(__dirname, "./src/pages"),
+  "@hooks": path.resolve(__dirname, "./src/hooks"),
+  "@utils": path.resolve(__dirname, "./src/utils"),
+  "@lib": path.resolve(__dirname, "./src/lib"),
+  "@styles": path.resolve(__dirname, "./src/styles"),
+  "@assets": path.resolve(__dirname, "./src/assets"),
+  "@router": path.resolve(__dirname, "./src/router"),
+};
+
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-      "@components": path.resolve(__dirname, "./src/components"),
-      "@features": path.resolve(__dirname, "./src/features"),
-      "@pages": path.resolve(__dirname, "./src/pages"),
-      "@hooks": path.resolve(__dirname, "./src/hooks"),
-      "@utils": path.resolve(__dirname, "./src/utils"),
-      "@lib": path.resolve(__dirname, "./src/lib"),
-      "@styles": path.resolve(__dirname, "./src/styles"),
-      "@assets": path.resolve(__dirname, "./src/assets"),
-      "@router": path.resolve(__dirname, "./src/router"),
+    alias: aliases,
+  },
+  test: {
+    globals: true,
+    environment: "jsdom",
+    setupFiles: "./vitest.setup.js",
+    css: true,
+    resolve: {
+      alias: aliases,
     },
   },
 });

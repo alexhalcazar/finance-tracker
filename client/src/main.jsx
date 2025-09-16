1d0b5daeba16887d1721756d68676ba81e2e79ca
jay.develops <swejesusperez@gmail.com>
2025-09-16 12:47:06 -0700

    chore: removed import on react

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,7 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
 import { RouterProvider } from "react-router/dom";
-import router from "@/router/routes"
+import router from "@/router/routes";
 
 const root = document.getElementById("root");
 
-ReactDOM.createRoot(root).render(
-  <RouterProvider router={router} />,
-);
+ReactDOM.createRoot(root).render(<RouterProvider router={router} />);

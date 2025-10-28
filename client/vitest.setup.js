2be2c2d2e8777350980b9b58df6a4d1567245893
KMTrochez <KMTrochez94@gmail.com>
2025-10-27 22:38:39 -0700

    Updated to test logout

--- client/vitest.setup.js
+++ client/vitest.setup.js
@@ -1,1 +1,5 @@
 import "@testing-library/jest-dom/vitest";
+// Set environment variables for testing
+process.env.JWT_ACCESS_SECRET = "test-secret-key-for-testing";
+process.env.JWT_ACCESS_TTL = "15m";
+process.env.BCRYPT_ROUNDS = "10";

bd17da5a8b5c41a18ed82a4bc081eecdd2d0651b
alexhalcazar <alexhalcazar369@gmail.com>
2025-10-29 12:01:16 -0700

    chore: update comments and annotate access token must be pulled from db

--- server/src/services/plaidService.js
+++ server/src/services/plaidService.js
@@ -1,8 +1,6 @@
 import moment from "moment";
 import "dotenv/config";
 import { plaidClient } from "#plaid";
-// placeholder until can retrieve access token from database
-let access_token = null;
 
 export const createLinkToken = async (user_id) => {
   const request = {
@@ -27,19 +25,13 @@ export const createLinkToken = async (user_id) => {
 };
 
 export const setAccessToken = async (publicToken) => {
-  let result = { error: "could not create access token" };
   try {
     const response = await plaidClient.itemPublicTokenExchange({
       public_token: publicToken,
     });
-    // TODO: store accessToken securely in our database
 
-    // placeholder
-    access_token = response.data.access_token;
-    if (access_token) {
-      result = { success: "access token created" };
-    }
-    return result;
+    const access_token = response.data.access_token;
+    // TODO: store accessToken securely in our database
   } catch (err) {
     console.error(
       "Could not exchange public token for access token:",
@@ -49,22 +41,18 @@ export const setAccessToken = async (publicToken) => {
   }
 };
 
-export const fetchTransactions = async (user_id, days = 30) => {
+export const fetchTransactions = async (access_token, days = 30) => {
   try {
     const now = moment();
     const today = now.format("YYYY-MM-DD");
     const pastDaysAgo = now.subtract(days, "days").format("YYYY-MM-DD");
 
-    // TODO: grab access token from database for user_id
     const response = await plaidClient.transactionsGet({
       access_token,
       start_date: pastDaysAgo,
       end_date: today,
     });
     const transactions = response.data.transactions;
-    console.log(
-      `You have ${transactions.length} transactions from the last thirty days.`
-    );
     return transactions;
   } catch (err) {
     console.error(

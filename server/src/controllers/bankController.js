fa4cca68ea3806e3970bfece52561822d89e9def
alexhalcazar <alexhalcazar369@gmail.com>
2025-11-03 09:11:53 -0800

    feat(access_token): temporarily extract access_token from req body

--- server/src/controllers/bankController.js
+++ server/src/controllers/bankController.js
@@ -1,14 +1,12 @@
 import { fetchTransactions } from "#services/plaidService";
 
 export const getTransactions = async (req, res) => {
-  const { user_id } = req.user.user_id;
-
+  // const { user_id } = req.user;
+  const { access_token } = req.body;
   try {
     // TODO: fetch the Plaid access_token from DB using the user_id
     // const access_token = await getAccessToken(user_id);
 
-    // place holder
-    const access_token = null;
     // can pass in amount of days
     const transactions = await fetchTransactions(access_token);
     res.json(transactions);

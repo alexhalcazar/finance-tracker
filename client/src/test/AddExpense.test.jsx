15238b9bc7924bc64525a9719774821891a5efb9
almacampos22 <almacampos2000.com>
2025-11-16 22:24:05 -0800

    chore:remove unnecessary import

--- client/src/test/AddExpense.test.jsx
+++ client/src/test/AddExpense.test.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { render, screen, waitFor } from "@testing-library/react";
 import { expect, test, describe, vi, beforeEach, afterEach } from "vitest";
 import userEvent from "@testing-library/user-event";

5d6eb21df96c08155fa90d0e95f1d2a5d8800dfa
jay.develops <swejesusperez@gmail.com>
2025-12-02 12:52:00 -0800

    feat: add crud operations for transaction API routes

--- server/src/routes/transactions.js
+++ server/src/routes/transactions.js
@@ -1,9 +1,16 @@
 import express from "express";
 import { verifyToken } from "#middleware/jwt";
-import { createNewTransaction } from "#controllers/transactionController";
+import {
+  createNewTransaction,
+  getTransaction,
+  updateTransaction,
+  deleteTransaction,
+} from "#controllers/transactionController";
 
 const router = express.Router();
 
 router.post("/", verifyToken, createNewTransaction);
-
+router.get("/:transaction_id?", verifyToken, getTransaction);
+router.put("/:transaction_id", verifyToken, updateTransaction);
+router.delete("/:transaction_id", verifyToken, deleteTransaction);
 export default router;

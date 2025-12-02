64328bb73b74d69542c5b8ab16b253d48eaa2693
jay.develops <swejesusperez@gmail.com>
2025-12-02 13:12:26 -0800

    test: add test for POST on recurring transaction

---
+++ server/tests/routes/recurringTransactions/recurringTransactions.test.js
@@ -0,0 +1,90 @@
+import { describe, expect, it, beforeAll, beforeEach, afterAll } from "vitest";
+import { generateAccessToken } from "src/utils/jwtUtils";
+import db from "#db";
+import app from "#app";
+import supertest from "supertest";
+
+describe("Transaction Routes", () => {
+  let testUser;
+  let testBudget;
+  let testCategory;
+  let authToken;
+  let request = null;
+
+  // hard coded category and recurring transaction data refered at global
+  const categoryData = {
+    name: "Utilities",
+    type: "expense",
+    limit: 500.0,
+    color: "#0000FF",
+  };
+
+  const recurringTransactionData = {
+    amount: 100.0,
+    note: "test note",
+    start_date: "2025-01-01",
+    end_date: "2026-01-01",
+    frequency: "monthly",
+  };
+
+  beforeAll(async () => {
+    await db.seed.run();
+
+    testUser = await db("users").first();
+    request = supertest.agent(app);
+
+    // Generate auth token
+    authToken = generateAccessToken({
+      user_id: testUser.user_id,
+      email: testUser.email,
+      username: testUser.username,
+    });
+
+    testBudget = await db("budgets")
+      .insert({
+        user_id: testUser.user_id,
+        name: "Test Budget",
+        start_date: "2025-01-01",
+        end_date: "2025-12-31",
+        currency: "USD",
+      })
+      .returning("*");
+
+    testCategory = await db("categories")
+      .insert({
+        ...categoryData,
+        budget_id: testBudget[0].budget_id,
+      })
+      .returning("*");
+  });
+
+  beforeEach(async () => {
+    await db("transactions").del();
+  });
+
+  afterAll(async () => {
+    await db("categories").del();
+    await db("budgets").del();
+    await db("users").del();
+    await db.destroy();
+  });
+
+  describe("POST /api/recurring-transactions", () => {
+    it("should create a new recurring transaction", async () => {
+      const response = await request
+        .post("/api/recurring-transactions")
+        .set("Authorization", `Bearer ${authToken}`)
+        .send({
+          ...recurringTransactionData,
+          category_id: testCategory[0].category_id,
+          budget_id: testBudget[0].budget_id,
+        });
+
+      expect(response.status).toBe(201);
+      expect(response.body.recurringTransaction).toHaveProperty("recurring_id");
+      expect(response.body.recurringTransaction.category_id).toBe(
+        testCategory[0].category_id
+      );
+    });
+  });
+});

45c22725c5935e25f05d8d8bbabc436d23effade
alexhalcazar <alexhalcazar369@gmail.com>
2025-12-07 13:19:10 -0800

    feat: create encryption/decryption for access token to/from database

--- server/src/services/plaidService.js
+++ server/src/services/plaidService.js
@@ -1,6 +1,8 @@
 import moment from "moment";
 import "dotenv/config";
 import { plaidClient } from "#plaid";
+import { encryptSymmetric, decryptSymmetric } from "#utils/encryption";
+import { selectUserById, updateUserById } from "#models/userModel";
 
 export const createLinkToken = async (user_id) => {
   const request = {
@@ -24,17 +26,23 @@ export const createLinkToken = async (user_id) => {
   }
 };
 
-export const setAccessToken = async (publicToken) => {
+export const setAccessToken = async (publicToken, user_id) => {
   try {
     const response = await plaidClient.itemPublicTokenExchange({
       public_token: publicToken,
     });
 
     const access_token = response.data.access_token;
-    // TODO: store accessToken securely in our database
-
-    // placeholder will return access_token for now
-    return access_token;
+    // store accessToken securely in database
+    const cipherObject = encryptSymmetric(access_token);
+    const { ciphertext, iv, tag } = cipherObject;
+    const updateObject = {
+      user_id,
+      encrypted_token: ciphertext.toString("base64"),
+      initialization_vector: iv.toString("base64"),
+      tag: tag.toString("base64"),
+    };
+    await updateUserById(updateObject);
   } catch (err) {
     console.error(
       "Could not exchange public token for access token:",
@@ -44,6 +52,25 @@ export const setAccessToken = async (publicToken) => {
   }
 };
 
+export const retrieveAccessToken = async (user_id) => {
+  try {
+    const user = await selectUserById(user_id);
+    const { encrypted_token, initialization_vector, tag } = user;
+    const access_token = decryptSymmetric(
+      encrypted_token,
+      initialization_vector,
+      tag
+    );
+
+    return access_token;
+  } catch (err) {
+    console.error(
+      "Could not retrieve access token from database",
+      err.response?.data || err.message
+    );
+    throw new Error("Error retrieving access token from database");
+  }
+};
 export const fetchTransactions = async (access_token, days = 30) => {
   try {
     const now = moment();

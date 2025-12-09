db800209a6155a4287ba0f9cebbb2c037dcd2414
jay.develops <swejesusperez@gmail.com>
2025-12-09 10:53:58 -0800

    feat: add logic to grab all total income from user in a summary route

---
+++ server/src/controllers/summaryController.js
@@ -0,0 +1,28 @@
+// summaryController.js handles grabbing global summary information
+
+const getTotalGlobalIncome = async (req, res) => {
+  const { user_id } = req.user;
+
+  try {
+    if (!user_id) {
+      return res.status(400).json({ error: "User is not authorized" });
+    }
+
+    const result = await knex("transactions")
+      .join("budgets", "transactions.budget_id", "budgets.budget_id")
+      .join("categories", "transactions.category_id", "categories.category_id")
+      .where("budgets.user_id", user_id)
+      .where("categories.type", "income")
+      .sum("transactions.amount as total_income")
+      .first();
+
+    res.status(200).json({
+      user_id,
+      total_income: result?.total_income || 0,
+    });
+  } catch (error) {
+    res.status(500).json({ error: "Error fetching income for user" });
+  }
+};
+
+export { getTotalGlobalIncome };

bf1720843749f3ff7f54f519dbefc071eb36d45e
alexhalcazar <alexhalcazar369@gmail.com>
2025-12-09 09:15:47 -0800

    fix: move /budget route above dynamic category_id to prevent incorrect matching

--- server/src/routes/categories.js
+++ server/src/routes/categories.js
@@ -10,8 +10,8 @@ import {
 
 const router = express.Router();
 
+router.get("/budget/:budget_id?", getAllCategories);
 router.get("/:category_id", getCategory);
-router.get("/budget/:budget_id", getAllCategories);
 router.post("/budget/:budget_id", createNewCategory);
 router.put("/:category_id", updateCategory);
 router.delete("/:category_id", deleteCategory);

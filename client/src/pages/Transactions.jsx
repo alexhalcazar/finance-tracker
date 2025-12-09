d5ce73948a186c0f07a449d2e6c7b27bb742fae1
alexhalcazar <alexhalcazar369@gmail.com>
2025-12-09 00:41:35 -0800

    chore(transactions): currently work in progress

--- client/src/pages/Transactions.jsx
+++ client/src/pages/Transactions.jsx
@@ -5,15 +5,27 @@ import {
   transactionLogo,
 } from "@components/config/sidebarConfig";
 import { Button } from "@/components/ui/button";
-import { useNavigate } from "react-router";
 import { Card } from "@/components/ui/Card";
 import { fetchLatestTransactions } from "@/api/plaid";
 import { useState } from "react";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { AddExpenseForm } from "@/features/expenses/AddExpenseForm";
+import { addExpenseSchema } from "@/formSchemas/allFormSchemas";
+import { useForm } from "react-hook-form";
 
 export const Transactions = ({ className }) => {
   const [transactions, setTransactions] = useState([]);
   const [error, setError] = useState(false);
-  const navigate = useNavigate();
+
+  const expenseForm = useForm({
+    resolver: zodResolver(addExpenseSchema),
+    defaultValues: {
+      amount: "",
+      category: "",
+      description: "",
+      transactionDate: "",
+    },
+  });
 
   const handleClick = async () => {
     try {
@@ -27,6 +39,28 @@ export const Transactions = ({ className }) => {
     }
   };
 
+  const onSubmit = async (budget) => {
+    const { name, start_date, end_date, currency } = budget;
+    try {
+      const response = await fetch("/api/budgets?limit=10", {
+        method: "POST",
+        headers: {
+          Authorization: `Bearer ${token}`,
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ name, start_date, end_date, currency }),
+      });
+      const data = await response.json();
+      const budget = data.newBudget;
+
+      setBudgets((prev) => [...prev, budget]);
+
+      form.reset();
+    } catch (err) {
+      console.error("Error creating a new budget:", err);
+    }
+  };
+
   return (
     <main className="flex flex-row min-h-screen">
       <Sidebar
@@ -35,10 +69,46 @@ export const Transactions = ({ className }) => {
         className={sideBarClass}
       />
       <Card className="m-4 p-4 flex-1 bg-gray-300 rounded-lg">
+        <h2 className="text-2xl font-bold mb-6">Transactions</h2>
+        <div className="flex flex-col md:flex-row gap-6">
+          <div className="flex-1 bg-white p-4 rounded-lg shadow-md">
+            <h3 className="text-xl font-semibold mb-4">Add New Transaction</h3>
+            <AddExpenseForm
+              formType="add"
+              form={expenseForm}
+              onSubmit={onSubmit}
+              isLoading={false}
+              className="space-y-4"
+            />
+          </div>
+
+          <div className="flex-1 bg-white p-4 rounded-lg shadow-md">
+            <h3 className="text-xl font-semibold mb-4">
+              Existing Transactions
+            </h3>
+            {/* {!budgets ? (
+              <p>Something went wrong</p>
+            ) : budgets.length === 0 ? (
+              <p className="text-gray-500">No budgets yet.</p>
+            ) : (
+              <ul className="space-y-2">
+                {budgets.map((budget, index) => (
+                  <li
+                    key={index}
+                    className="border p-2 rounded-md flex justify-between"
+                  >
+                    <span>
+                      <strong>{budget.budget_id}:</strong> {budget.name}
+                    </span>
+                    <span>${parseFloat(budget.currency).toFixed(2)}</span>
+                  </li>
+                ))}
+              </ul>
+            )} */}
+          </div>
+        </div>
+
         <div className="flex flex-wrap gap-4 justify-around">
-          <Button onClick={() => navigate("/transactions/add-expense")}>
-            Add an expense
-          </Button>
           <Button onClick={handleClick}>Get transactions</Button>
         </div>
         <div className="flex flex-col items-center w-full">
